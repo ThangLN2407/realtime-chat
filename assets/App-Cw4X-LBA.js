27f04ab36014a5d0a9f90a28ff58cf47683da953
ThangLN2407 <ThangLN2407@users.noreply.github.com>
2025-05-05 12:35:28 +0000

    deploy: 8f8ac5253c350047313d870b258b2c4171b30ded

---
+++ assets/App-Cw4X-LBA.js
@@ -0,0 +1,1 @@
+import{j as s,O as e}from"./index-DX_eVN3k.js";function t(){return s.jsx("div",{className:"min-h-screen bg-gray-50",children:s.jsx(e,{})})}export{t as default};
---
+++ assets/Login-6uVZ8wOI.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DX_eVN3k.js";import{A as t}from"./AuthForm-ewviWtyh.js";import"./index-CLMHX5u9.js";import"./index-MmqF42aa.js";const m=()=>r.jsx("div",{className:"flex items-center justify-center h-screen bg-gray-100",children:r.jsx(t,{isLogin:!0})});export{m as default};
