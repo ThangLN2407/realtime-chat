d8e7532bf9de292a4784e3576cb39f52418aa86f
Le Ngoc Thang <thangln@bap.jp>
2025-05-10 15:52:17 +0700

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/realtime-chat/',
+  base: '/realtime-chat',
   plugins: [react(),  tailwindcss(),],
 })
